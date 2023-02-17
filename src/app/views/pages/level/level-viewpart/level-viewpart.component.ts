0a0e827f37db401ba91fd7d04222f7bd8fccfa22
ShahulHameed <shahulhameed.a@xenovex.com>
2023-02-17 14:55:25 +0530

    completed

--- src/app/views/pages/level/level-viewpart/level-viewpart.component.ts
+++ src/app/views/pages/level/level-viewpart/level-viewpart.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild, ElementRef, ViewChildren, QueryList } from '@angular/core';
+import { NgModel } from '@angular/forms';
+import {BrowserModule, DomSanitizer} from '@angular/platform-browser'
 
 @Component({
   selector: 'app-level-viewpart',
@@ -6,10 +8,76 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./level-viewpart.component.scss']
 })
 export class LevelViewpartComponent implements OnInit {
+  @ViewChildren('jslist') jslist: QueryList<ElementRef>;
 
-  constructor() { }
+  viewPage: number = 1;
+
+  getQustion: any[];
+  jsonList: any[];
+  answerBox: boolean = false;
+
+  constructor(
+    public sanitizer: DomSanitizer
+  ) { }
 
   ngOnInit(): void {
+    this.initJsonList();
+    
+  }
+
+  initJsonList(){
+    this.jsonList = [
+      {
+        "id":'1',
+        "question":'It is a long established fact <span class="input-bx" (drop)="drop($event)" pDroppable></span> a reader will be distracted by <span class="input-bx" (drop)="drop($event)" pDroppable></span> readable content of a page <span class="input-bx" (drop)="drop($event)" pDroppable></span> looking at its layout.',
+        "crctques":'It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout.',
+        "ops_list":['that','the','when'],
+        "getques":'',
+      },
+      {
+        "id":'2',
+        "question":'It is a long established fact <span class="input-bx" (drop)="drop($event)" pDroppable></span> a reader will be distracted by <span class="input-bx" (drop)="drop($event)" pDroppable></span> readable content of a page <span class="input-bx" (drop)="drop($event)" pDroppable></span> looking at its layout.',
+        "crctques":'It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout.',
+        "ops_list":['that','the','when'],
+        "getques":'',
+      }
+    ]
+  }  
+
+  allowDrop(ev){
+    ev.preventDefault();
+  }
+
+  drag(ev){
+    ev.dataTransfer.setData("Text",ev.target.id);
+  }
+
+  drop(ev){
+    ev.preventDefault();
+    var data=ev.dataTransfer.getData("Text");
+    ev.target.parentNode.replaceChild(document.getElementById(data), ev.target);
+    document.getElementById(data).className = "droped_cls";
+  }
+
+  getValue(){
+    // this.getQustion = [];
+    // console.log('"Get Value is" -> ',this.myNameElem.nativeElement.innerText);
+    // this.getQustion = this.jslist.nativeElement.innerText
+    this.answerBox = true;
+    this.jslist.forEach(i => {
+      // this.getQustion.push(i.nativeElement.innerText);
+      var splite_id = i.nativeElement.id.split('_')[1]
+      this.jsonList.forEach(j => {
+        if(splite_id == j.id){
+          j.getques = i.nativeElement.innerText;
+        }
+      })
+    });
+    console.log(this.jsonList,'this.jsonList')
+  }
+
+  gotoNextpage(e){
+    this.viewPage = e;
   }
 
 }
