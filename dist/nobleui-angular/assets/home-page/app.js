615b880d451525b0c7a31460209c9ccc615a43ee
Gourav More <gourav_m@tekditechnologies.com>
2023-06-23 12:46:45 +0530

    removed unwnated code

--- dist/nobleui-angular/assets/home-page/app.js
+++
@@ -1,8 +0,0 @@
-$('.moreless-button').click(function() {
-    $('.moretext').slideToggle();
-    if ($('.moreless-button').text() == "Read more") {
-      $(this).text("Read less")
-    } else {
-      $(this).text("Read more")
-    }
-  });
