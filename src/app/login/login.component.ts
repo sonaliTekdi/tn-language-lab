4c4440903a19f12652f13106b6e9a22b7c47e791
Ajinkya Pande <ajinkya.pande@tekditechnologies.com>
2023-06-28 12:33:02 +0530

    Login Ui Upgradation

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -13,6 +13,9 @@ export class LoginComponent {
   email: string;
   password: string;
   errorMessage: string;
+  loginError: boolean = false;  
+  passwordLength: number;
+  isPasswordVisible: boolean = false;
 
   constructor(public telemetryService: TelemetryService, private authService: AuthService, private router: Router) { }
 
@@ -27,11 +30,13 @@ export class LoginComponent {
         {
           this.telemetryService.response(jwt_decode(data?.records?.token));
           localStorage.setItem('token', data?.records?.token);
+          this.loginError = false;
         }else{
           this.telemetryService.error(data?.message, {
             err: data?.message, errtype: data?.status
           });
-          alert(data.message)
+          // alert(data.message)
+          this.loginError = true;
         }
 
     // Redirect to protected route
@@ -43,4 +48,18 @@ export class LoginComponent {
       }
     );
   }
+  calculatePasswordLength() {
+    this.passwordLength = this.password ? this.password.length : 0;
+  }
+  ngOnChanges() {
+    this.calculatePasswordLength();
+  }
+  getPasswordIcon() {
+    return this.passwordLength !== 0 ? '../../assets/images/eye.svg' : '../../assets/images/password.svg';
+  }
+
+  togglePasswordVisibility() {
+    this.isPasswordVisible = !this.isPasswordVisible;
+  }
+  
 }
