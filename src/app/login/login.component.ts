b1156cdd625aa5db264bb7cab601d9a631b47b41
Ajinkya Pande <ajinkya.pande@tekditechnologies.com>
2023-06-29 17:17:22 +0530

    added prefix

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -4,12 +4,10 @@ import { AuthService } from '../auth.service';
 import { TelemetryService } from '../telemetry.service';
 import { environment } from 'src/environments/environment';
 import { UserService } from '../user/user.service';
-
-
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.scss']
+  styleUrls: ['./login.component.scss'],
 })
 export class LoginComponent {
   email: string;
@@ -19,33 +17,27 @@ export class LoginComponent {
   passwordLength: number;
   isPasswordVisible: boolean = false;
 
-  constructor(public userService: UserService, public telemetryService: TelemetryService, private authService: AuthService, private router: Router) { }
+  constructor(
+    public userService: UserService,
+    public telemetryService: TelemetryService,
+    private authService: AuthService,
+    private router: Router
+  ) {}
 
   ngOnInit() {
-    this.telemetryService.impression("Login", "/login");
-        $(document).ready(function(){
-        $("#eng").click(function(){
-          localStorage.setItem('lang','en');
-          document.location.reload();
-        });
-        $("#de").click(function(){
-          localStorage.setItem('lang','ta');
-          document.location.reload();
-        });
-        });
+    this.telemetryService.impression('Login', '/login');
   }
-  loginAsGuest(){
+  loginAsGuest() {
     localStorage.removeItem('token');
-    this.telemetryService.interact("LoginAsGuest", 'Login')
+    this.telemetryService.interact('LoginAsGuest', 'Login');
     localStorage.setItem('guestUser', 'true');
     this.router.navigate(['/level']);
   }
   login() {
-    this.telemetryService.interact("Submit", 'Login')
+    this.telemetryService.interact('Submit', 'Login');
     this.authService.login(this.email, this.password).subscribe(
       (data) => {
-        if (data.dataStatus)
-        {
+        if (data.dataStatus) {
           localStorage.setItem('token', data?.records?.token);
           let users = this.userService.getUser();
           this.telemetryService.log(
@@ -58,20 +50,30 @@ export class LoginComponent {
           this.router.navigate(['/level']);
 
           this.loginError = false;
-        }else{
+        } else {
           this.telemetryService.error(data?.message, {
-            err: data?.message, errtype: data?.status
+            err: data?.message,
+            errtype: data?.status,
           });
           // alert(data.message)
           this.loginError = true;
         }
       },
-      error => {
-        console.log(error)
+      (error) => {
+        console.log(error);
         this.errorMessage = error.message;
       }
     );
   }
+  switchToEnglish() {
+    localStorage.setItem('lang', 'en');
+    document.location.reload();
+  }
+  switchToTamil() {
+    localStorage.setItem('lang', 'ta');
+    document.location.reload();
+  }
+
   calculatePasswordLength() {
     this.passwordLength = this.password ? this.password.length : 0;
   }
@@ -79,11 +81,12 @@ export class LoginComponent {
     this.calculatePasswordLength();
   }
   getPasswordIcon() {
-    return this.passwordLength !== 0 ? '../../assets/images/eye.svg' : '../../assets/images/password.svg';
+    return this.passwordLength !== 0
+      ? '../../assets/images/eye.svg'
+      : '../../assets/images/password.svg';
   }
 
   togglePasswordVisibility() {
     this.isPasswordVisible = !this.isPasswordVisible;
   }
-
 }
